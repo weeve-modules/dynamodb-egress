789a457321b98dd1258e2b4a356e7358f7002527
kubagrzelak <jakubgrzelak1@gmail.com>
2021-12-15 10:44:27 +0000

    Updated envs

--- image/src/app/config/weeve.py
+++ image/src/app/config/weeve.py
@@ -6,8 +6,13 @@ from app.utils.env import env
 WEEVE = {
     "MODULE_NAME": env("MODULE_NAME", "dynamodb-egress"),
     "MODULE_TYPE": env("MODULE_TYPE", "EGRESS"),
-    "EGRESS_URL": env("EGRESS_URL", "http://localhost:8000"),
+    "EGRESS_SCHEME": env("EGRESS_SCHEME", "http"),
+    "EGRESS_HOST": env("EGRESS_HOST", "localhost"),
+    "EGRESS_PORT": env("EGRESS_PORT", "80"),
+    "EGRESS_PATH": env("EGRESS_PATH", ""),
+    "EGRESS_URL": env("EGRESS_URL", ""),
     "INGRESS_HOST": env("INGRESS_HOST", "0.0.0.0"),
     "INGRESS_PORT": env("INGRESS_PORT", "80"),
-    "INGRESS_PATH": env("INGRESS_PATH", "/")
+    "INGRESS_PATH": env("INGRESS_PATH", "")
+
 }
