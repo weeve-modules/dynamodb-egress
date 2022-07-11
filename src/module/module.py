722cbe3fcc61820bc6217c32021fa82f052acfcd
kubagrzelak <jakubgrzelak1@gmail.com>
2022-07-11 21:37:49 +0100

    Migrated to V1.

--- src/module/module.py
+++ src/module/module.py
@@ -6,9 +6,22 @@ Edit this file to implement your module.
 """
 
 from logging import getLogger
+import boto3
+from time import time
+from decimal import Decimal
+from uuid import uuid4
+from .params import PARAMS
 
 log = getLogger("module")
 
+__AWS_ACCESS_KEY_ID__ = PARAMS['AWS_ACCESS_KEY_ID']
+__AWS_SECRET_ACCESS_KEY__ = PARAMS['AWS_SECRET_ACCESS_KEY']
+__REGION__ = PARAMS['REGION']
+__TABLE__ = PARAMS['TABLE']
+
+dynamodb = boto3.resource('dynamodb', region_name=__REGION__, aws_access_key_id=__AWS_ACCESS_KEY_ID__, aws_secret_access_key=__AWS_SECRET_ACCESS_KEY__)
+
+table = dynamodb.Table(__TABLE__)
 
 def module_main(received_data: any) -> str:
     """
@@ -27,8 +40,47 @@ def module_main(received_data: any) -> str:
 
     try:
         # YOUR CODE HERE
+        timestamp = int(time())
+        if(type(received_data) is list):
+            write_batch(received_data, timestamp)
+        else:
+            table.put_item(Item=parse_for_dynamo(received_data, timestamp))
 
         return None
 
     except Exception as e:
         return f"Exception in the module business logic: {e}"
+
+def convert_to_decimal(data):
+    """
+    Converts all float values in a dict to Decimal Type
+    Args:
+        data (JSON Object): data to transform
+    Returns:
+        data (JSON Object): Transform data
+    """
+    for key in data:
+        if type(data[key]) == float:
+            data[key] = Decimal(str(data[key]))
+        if type(data[key]) == dict:
+            data[key] = convert_to_decimal(data[key])
+    return data
+
+
+def parse_for_dynamo(data, timestamp: int):
+    data["id"] = str(uuid4())
+    if not "timestamp" in data:
+        data['timestamp'] = timestamp
+    return convert_to_decimal(data)
+
+
+def write_batch(dataList: list, timestamp: int):
+    """
+    Writes batch data to dynamodb
+    Args:
+        dataList ([JSON Object]): [data to batch write]
+        timestamp (timestamp): timestamp
+    """
+    with table.batch_writer() as batch:
+        for data in dataList:
+            batch.put_item(Item=parse_for_dynamo(data, timestamp))
